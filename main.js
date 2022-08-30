6c5b0e4f1a41abe18ce188a9a2b233ff2f4c9efd
d <me@deetercesler.com>
2022-08-30 14:29:11 -0400

    time increments correctly at any range

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
 const el = document.getElementById('num')
 const goalValue = el.innerText
-console.log('goal')
-console.log(goalValue)
 
 // if no time attr given, defaul to 2 seconds
-const time = parseFloat(el.getAttribute('time')) || 2
-const milliseconds = time * 1000
+const seconds = parseFloat(el.getAttribute('time')) || 2
+const milliseconds = seconds * 1000
 
 const num = parseInt(el.innerText)
 
-// if num > time in milliseconds, divided it by # of milliseconds and increment it
-// each time by that divider (checking and making sure the times line up)
+/*
+ *  If num > time in milliseconds, divided it by # of milliseconds and increment it
+ *  each time by that divider (checking and making sure the times line up).
+ *
+ *  Also, most browsers can't process setInterval faster than 16 milliseconds per interval,
+ *  so to account for this I set interval time 16 and divide the incrementor by that.
+ */
+
 let incrementor = 0
 el.innerText = incrementor
-if (num >= milliseconds) {
-  const chunkedIncrementor = parseInt(num / (time * 100))
+const browserLimitation = 16
+const timeDivider = 1000 / browserLimitation
+
+if (num >= milliseconds / browserLimitation) {
+  const chunkedIncrementor = parseInt(num / (seconds * timeDivider))
   setInterval(() => {
     if (incrementor < num) {
       incrementor += chunkedIncrementor
       el.innerText = incrementor
       if (incrementor > goalValue) {
-        //   console.log('test33')
-        //   console.log(el.innerText)
-        //   console.log(goalValue)
         el.innerText = goalValue
       }
     }
-  }, 10)
-} else if (num < milliseconds) {
+  }, browserLimitation)
+} else {
   const incrementMilliseconds = milliseconds / num
+  console.log(incrementMilliseconds)
   setInterval(() => {
     if (incrementor < num) {
       ++incrementor
       el.innerText = incrementor
     }
-  }, incrementMilliseconds)
+  }, browserLimitation)
 }
