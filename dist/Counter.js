e91c3a52f8bac0ff827f7b0ea4a0e9ea6d4a642e
d <me@deetercesler.com>
2022-09-30 14:14:13 -0400

    update build

--- dist/Counter.js
+++ dist/Counter.js
@@ -1,26 +1,19 @@
 import React, { useEffect, useState, useRef } from 'react';
 export default function Counter(props) {
   const [number, setNum] = useState(0);
-  const [renderCount, setCount] = useState(0);
-  const reffer = useRef(0);
+  const ref = useRef(0);
   const el = props.children;
   const goalValue = parseInt(el); // take el.innerText
   // run the function against it
   // reset innertext as what I output
 
   useEffect(() => {
-    console.log('!');
     increment();
   }, []);
 
   const increment = () => {
-    console.log('ciao bella!');
-    console.log('goalValue');
-    console.log(goalValue); // setCount(renderCount++)
-    // console.log(++renderCount)
     // if no time attr given, default to 2 seconds
-
-    const seconds = parseFloat(props.time) || 10;
+    const seconds = parseFloat(props.time) || 2;
     const milliseconds = seconds * 1000;
     /*
      *  If goalValue > time in milliseconds, divided it by # of milliseconds and increment it
@@ -31,12 +24,11 @@ export default function Counter(props) {
      */
 
     let incrementor = 0;
-    reffer.current = incrementor;
+    ref.current = incrementor;
     const browserLimitation = 16;
     const timeDivider = 1000 / browserLimitation; // If the number is huge and needs to count quickly
 
     if (goalValue >= milliseconds / browserLimitation) {
-      console.log('cazzo');
       const chunkedIncrementor = parseInt(goalValue / (seconds * timeDivider));
       const counter = setInterval(() => {
         if (incrementor < goalValue) {
@@ -50,21 +42,14 @@ export default function Counter(props) {
         }
       }, browserLimitation);
     } else {
-      console.log('ma donna'); // If the number is tiny and it needs to count slowly
-
+      // If the number is tiny and it needs to count slowly
       const incrementMilliseconds = milliseconds / goalValue;
-      console.log('-1');
       const counter = setInterval(() => {
-        console.log('0');
-
         if (incrementor < goalValue) {
           ++incrementor;
-          console.log('incre');
-          console.log(incrementor);
           setNum(Number(incrementor).toLocaleString());
 
           if (incrementor >= goalValue) {
-            console.log('.......');
             setNum(Number(goalValue).toLocaleString());
             clearInterval(counter);
           }
@@ -74,6 +59,6 @@ export default function Counter(props) {
   };
 
   return /*#__PURE__*/React.createElement("span", {
-    ref: reffer
+    ref: ref
   }, number);
 }
